cb57e921d47bec94cf18585e103082f77c6b98c9
John Lewis <jalewis1221@gmail.com>
2020-03-25 17:11:33 -0500

    added heroku

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4444/graphql"
+  uri: "http://coronavirus-react.herokuapp.com/graphql"
 });
 
 const Root = () => (
