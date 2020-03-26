4f11f35a8546ce7d4a261a7642571df068b82c67
John Lewis <jalewis1221@gmail.com>
2020-03-25 19:51:39 -0500

    changes

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ const port = process.env.PORT || 4444;
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true
   })
   .then(() => console.log("DB Conected"))
   .catch(err => console.error(err));
